78688fa38d5f345183b1c5921e91d15b3b558347
amontalvof <amontalvof92@gmail.com>
2021-05-11 11:59:47 -0500

    feat: prepare desktop screen

--- public/js/desk.js
+++ public/js/desk.js
@@ -1,1 +1,32 @@
-console.log('Desk HTML');
+// HTML References
+const lblDesk = document.querySelector('h1');
+const btnAttend = document.querySelector('button');
+
+const searchParams = new URLSearchParams(window.location.search);
+if (!searchParams.has('desk')) {
+    window.location = 'index.html';
+    throw new Error('The desk is required');
+}
+
+const desk = searchParams.get('desk');
+lblDesk.innerText = desk;
+
+const socket = io();
+
+socket.on('connect', () => {
+    btnAttend.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAttend.disabled = true;
+});
+
+socket.on('last-ticket', (lastTicket) => {
+    // lblNewTicket.innerText = `Ticket ${lastTicket}`;
+});
+
+btnAttend.addEventListener('click', () => {
+    // socket.emit('next-ticket', null, (ticket) => {
+    //     lblNewTicket.innerText = ticket;
+    // });
+});
